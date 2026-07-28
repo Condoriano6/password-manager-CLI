56c7a840f0677d892cf3af725ea272708996eaa3
Amirreza <arz86@yahoo.com>
2026-07-28 12:05:16 +0330

    Update credential.py
    added Credental class and __init__

--- credential.py
+++ credential.py
@@ -1,3 +1,11 @@
 '''
-credential
+Represents a single credential stored in the password manager.
 '''
+
+class Credential:
+    def __init__(self, cred_id, website, username, email, password):
+        self.cred_id = cred_id
+        self.website = website
+        self.username = username
+        self.email = email
+        self.password = password
