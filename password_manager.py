bcfb8a65a3a88e5db8431637d58b13ee1f29bbe4
Amirreza <arz86@yahoo.com>
2026-07-28 12:06:58 +0330

    Update password_manager.py
    Structure of the file

--- password_manager.py
+++ password_manager.py
@@ -1,1 +1,35 @@
+'''
+Represents the actions that user can do on credentials
+'''
 
+from colorama import Fore, Style
+
+from credential import Credential
+
+class PasswordManager:
+    def __init__(self):
+        self.credentials = []
+
+    def search_by_id(self):
+        pass
+    
+    def add_credential(self):
+        pass
+
+    def remove_credential(self):
+        pass
+
+    def edit_credential(self):
+        pass
+
+    def show_menu(self):
+        pass
+
+    def sort_credential(self):
+        pass
+
+    def filter_credential(self):
+        pass
+
+    def show_credentials(self, credentials=None):
+        pass
